88bebd231e8cfac89f8f9f7970acee38929c34bd
Hana Sambora <grace.sambora@gmail.com>
2023-08-16 17:18:08 -0500

    Complete lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,14 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */
+        projects.map(project => {
+          const {id, name, about, technologies} = project;
+          return <ProjectItem key={id} name={name} about={about} technologies={technologies} />
+        })
+      }</div>
     </div>
-  );
-}
+  );}
+
 
 export default ProjectList;
